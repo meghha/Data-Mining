1402f5edad1dc166ec79b1fb938f2b7b41042232
meghha <84433333+meghha@users.noreply.github.com>
2024-01-25 11:15:56 -0500

    Update question6.py

--- question6.py
+++ question6.py
@@ -5,11 +5,23 @@ def question6_1():
         'equal_frequency': ['string', integer]
     }
     """
+    answer = {
+        'equal_width': ['No change', 9],
+        'equal_frequency': ['No change', 9]
+    }
     return answer
 
 def question6_2():
+    answer = {
+        'equal_width': ['Change', 4],
+        'equal_frequency': ['No change', 9]
+    }
     return answer
 
 def question6_3():
+    answer = {
+        'equal_width': ['Change', 4],
+        'equal_frequency': ['No change', 4]
+    }
     return answer 
 
