1adb72442f3b1446c40e55e68eb10864d8e55cf1
meghha <84433333+meghha@users.noreply.github.com>
2024-01-29 23:24:43 -0500

    Update question6.py

--- question6.py
+++ question6.py
@@ -13,7 +13,7 @@ def question6_1():
 
 def question6_2():
     answer = {
-        'equal_width': ['Change', 4],
+        'equal_width': ['No change', 9],
         'equal_frequency': ['No change', 9]
     }
     return answer
