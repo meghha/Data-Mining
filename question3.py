87377c952878a0415b10b6fe1fd0559649ce00ba
meghha <84433333+meghha@users.noreply.github.com>
2024-01-29 23:31:08 -0500

    Update question3.py

--- question3.py
+++ question3.py
@@ -20,7 +20,7 @@ def question3_5():
     return answer
 
 def question3_6():
-    answer = ['continuous','qualitative','ordinal']
+    answer = ['continuous','quantitative','ratio']
     return answer
 
 def question3_7():
